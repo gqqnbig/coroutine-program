3f0a5dce133f801e3d426fb4f067176b79e9e254
QQ喵 <gqqnb2005@gmail.com>
2023-02-09 20:34:57 +0800

    add Python open file, read line, and zip example

---
+++ Tests/PythonExampleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GeneratorCalculation;
+using Xunit;
+
+namespace GeneratorCalculation.Tests
+{
+	public class PythonExampleTests
+	{
+		[Fact]
+		public void Run()
+		{
+
+			List<Generator> coroutines = new List<Generator>();
+			coroutines.Add(new Generator("oc1", new GeneratorType((ConcreteType)"Y", ConcreteType.Void)));
+			coroutines.Add(new Generator("oc2", new GeneratorType((ConcreteType)"Y", ConcreteType.Void)));
+			coroutines.Add(new Generator("fr1", new GeneratorType(new ListType((ConcreteType)"S", PaperStar.Instance), (ConcreteType)"Y")));
+			coroutines.Add(new Generator("fr2", new GeneratorType(new ListType((ConcreteType)"S", PaperStar.Instance), (ConcreteType)"Y")));
+			coroutines.Add(new Generator("zip", new GeneratorType(new ListType(new SequenceType((PaperVariable)"x", (PaperVariable)"y"), new FunctionType("min", (PaperVariable)"m", (PaperVariable)"n")), new SequenceType(new ListType((PaperVariable)"x", (PaperVariable)"m"), new ListType((PaperVariable)"y", (PaperVariable)"n")))));
+
+			var result = Solver.Solve(coroutines);
+
+			Assert.Equal(ConcreteType.Void, result.Receive);
+			Assert.IsType<ListType>(result.Yield);
+			var l = result.Yield as ListType;
+			Assert.IsType<FunctionType>(l.Size);
+			Assert.IsType<SequenceType>(l.Type);
+		}
+	}
+}
