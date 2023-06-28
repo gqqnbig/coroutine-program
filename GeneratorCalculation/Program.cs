54ece2837c4be710fef84ca663db9d1adb68b3b8
QQ喵 <gqqnb2005@gmail.com>
2023-06-28 20:58:26 +0800

    change test

--- GeneratorCalculation/Program.cs
+++ GeneratorCalculation/Program.cs
@@ -9,19 +9,14 @@ namespace GeneratorCalculation
 		static void Main(string[] args)
 		{
 			var coroutines = new List<Generator>();
-			coroutines.Add(new Generator("createItem", new GeneratorType((ConcreteType)"Item", ConcreteType.Void)));
-
-			coroutines.Add(new Generator("createStore", new GeneratorType((ConcreteType)"Store", ConcreteType.Void)));
-			coroutines.Add(new Generator("createCashDesk", new GeneratorType((ConcreteType)"CashDesk", ConcreteType.Void)));
-			coroutines.Add(new Generator("openStore", new GeneratorType(new SequenceType((ConcreteType)"Store", (ConcreteType)"CurrentStore"), (ConcreteType)"Store")));
-			coroutines.Add(new Generator("openCashDesk", new GeneratorType(new SequenceType((ConcreteType)"CashDesk", (ConcreteType)"CurrentStore", (ConcreteType)"CurrentCashDesk"), new SequenceType((ConcreteType)"CashDesk", (ConcreteType)"CurrentStore"))));
-
-			coroutines.Add(new Generator("makeNewSale", new GeneratorType(new SequenceType((ConcreteType)"Sale", (ConcreteType)"CurrentSale", (ConcreteType)"CurrentCashDesk"), (ConcreteType)"CurrentCashDesk")));
-			coroutines.Add(new Generator("enterItem", new GeneratorType(new SequenceType((ConcreteType)"SalesLineItem", (ConcreteType)"CurrentSale", (ConcreteType)"Item"),
-				new SequenceType((ConcreteType)"Item", (ConcreteType)"CurrentSale"))));
-			coroutines.Add(new Generator("endSale", new GeneratorType((ConcreteType)"CurrentSale", (ConcreteType)"CurrentSale")));
-			coroutines.Add(new Generator("makeCashPayment", new GeneratorType(new SequenceType((ConcreteType)"CurrentSale", (ConcreteType)"CashPayment"), (ConcreteType)"CurrentSale")));
-
+			coroutines.Add(new Generator("openStore", new CoroutineType(new SequenceType("Store"), new SequenceType("Store", "CurrentStore"))));
+			coroutines.Add(new Generator("openCashDesk", new CoroutineType(new SequenceType("CashDesk", "CurrentStore"), new SequenceType("CashDesk", "CurrentStore", "CurrentCashDesk"))));
+			coroutines.Add(new Generator("makeNewSale", new CoroutineType(new SequenceType("CurrentCashDesk"), new SequenceType("CurrentCashDesk", "Sale", "CurrentSale"))));
+			coroutines.Add(new Generator("enterItem", new CoroutineType(new SequenceType("CurrentSale","Item"), new SequenceType("CurrentSale", "Item", "SalesLineItem", "CurrentSaleLine"))));
+			coroutines.Add(new Generator("createStore", new CoroutineType(ConcreteType.Void, new SequenceType("Store"))));
+			coroutines.Add(new Generator("createCashDesk", new CoroutineType(ConcreteType.Void, new SequenceType("CashDesk"))));
+			coroutines.Add(new Generator("createItem", new CoroutineType(ConcreteType.Void, new SequenceType("Item"))));
+			
 
 			var result = new Solver().Solve(coroutines);
 			Console.WriteLine(result);
