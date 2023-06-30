8793ffa1d14dd8023a4931a1d1c1594a7b0dde8f
QQ喵 <gqqnb2005@gmail.com>
2023-06-30 16:57:56 +0800

    Add tool for building on Ubuntu
    The Command attribute uses sh syntax if the system is Linux.

---
+++ SmartContractAnalysis/prebuild.sh
@@ -0,0 +1,7 @@
+cd "${ProjectDir}"
+export CLASSPATH=".:${ProjectDir}antlr-4.13.0-complete.jar:${CLASSPATH}"
+alias antlr4='java org.antlr.v4.Tool'
+
+antlr4 -Dlanguage=CSharp -no-listener -visitor -o generated -package DiffSyntax.Antlr REModel.g4
+# antlr4.bat -Dlanguage=CSharp -no-listener -visitor -o generated -package DiffSyntax.Antlr  JavaLexer.g4 JavaParser.g4
+
