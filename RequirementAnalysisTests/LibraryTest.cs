20b5be4ff8816f5f14ab28f30a2169abc296e7c3
QQ喵 <gqqnb2005@gmail.com>
2023-11-16 16:24:16 +0800

    Add use case RecommendBook

--- RequirementAnalysisTests/LibraryTest.cs
+++ RequirementAnalysisTests/LibraryTest.cs
@@ -11,7 +11,7 @@ namespace RequirementAnalysis.Tests
 	public class LibraryTest
 	{
 		[Fact]
-		public static void Run()
+		public static void BorrowBook()
 		{
 			string content;
 			var assembly = typeof(LibraryTest).GetTypeInfo().Assembly;
@@ -58,6 +58,48 @@ namespace RequirementAnalysis.Tests
 			coroutines.Add(new Generator("", new GeneratorType(new TupleType(from b in bindings select b.Key), ConcreteType.Void)));
 			coroutines.AddRange(generators.Where(g => Array.IndexOf(lowPriorityCoroutines, g.Name) != -1));
 
+
+
+			var result = new Solver().SolveWithBindings(coroutines, bindings);
+			Console.WriteLine(result);
+		}
+
+
+		[Fact]
+		public static void RecommendBook()
+		{
+			string content;
+			var assembly = typeof(LibraryTest).GetTypeInfo().Assembly;
+			var file = assembly.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith("library.remodel"));
+
+			using (var stream = typeof(CocomeTest).GetTypeInfo().Assembly.GetManifestResourceStream(file))
+			{
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					content = reader.ReadToEnd();
+				}
+			}
+
+			var inheritance = REModelStart.GetObjectInheritance(content);
+			var generators = REModelStart.GetAllGenerators(content, inheritance);
+
+			string[] interestedCoroutines =
+			{
+				"ManageUserCRUDService::createFaculty",
+				"ManageUserCRUDService::createStudent",
+				"LibraryManagementSystemSystem::recommendBook",
+				"ListBookHistory::listRecommendBook",
+			};
+
+			var bindings = new Dictionary<PaperVariable, PaperWord>();
+			foreach (var g in generators.Where(g => Array.IndexOf(interestedCoroutines, g.Name) != -1))
+			{
+				bindings.Add(g.Name, g.Type);
+			}
+
+			var coroutines = new List<Generator>();
+
+			coroutines.Add(new Generator("", new GeneratorType(new TupleType(from b in bindings select b.Key), ConcreteType.Void)));
 
 
 			var result = new Solver().SolveWithBindings(coroutines, bindings);
