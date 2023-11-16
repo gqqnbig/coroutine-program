56205b214620f71834fa832d5d80906576e803b9
QQ喵 <gqqnb2005@gmail.com>
2023-11-16 17:08:59 +0800

    Add test PlaceOrder

--- RequirementAnalysisTests/CocomeTest.cs
+++ RequirementAnalysisTests/CocomeTest.cs
@@ -11,7 +11,7 @@ namespace RequirementAnalysis.Tests
 	public class CocomeTest
 	{
 		[Fact]
-		public static void Run()
+		public static void MakeNewSale()
 		{
 			string content;
 			var assembly = typeof(CocomeTest).GetTypeInfo().Assembly;
@@ -49,6 +49,56 @@ namespace RequirementAnalysis.Tests
 				"ManageCashDeskCRUDService::deleteCashDesk",
 			};
 
+
+
+			var bindings = new Dictionary<PaperVariable, PaperWord>();
+			foreach (var g in generators.Where(g => Array.IndexOf(interestedCoroutines, g.Name) != -1))
+			{
+				bindings.Add(g.Name, g.Type);
+			}
+
+			var coroutines = new List<Generator>();
+
+			coroutines.Add(new Generator("", new GeneratorType(new TupleType(from b in bindings select b.Key), ConcreteType.Void)));
+			coroutines.AddRange(generators.Where(g => Array.IndexOf(lowPriorityCoroutines, g.Name) != -1));
+
+
+
+			var result = new Solver().SolveWithBindings(coroutines, bindings);
+			Console.WriteLine(result);
+		}
+
+
+		[Fact]
+		public static void PlaceOrder()
+		{
+			string content;
+			var assembly = typeof(CocomeTest).GetTypeInfo().Assembly;
+			var file = assembly.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith("cocome.remodel"));
+
+			using (var stream = typeof(CocomeTest).GetTypeInfo().Assembly.GetManifestResourceStream(file))
+			{
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					content = reader.ReadToEnd();
+				}
+			}
+
+			var inheritance = REModelStart.GetObjectInheritance(content);
+			var generators = REModelStart.GetAllGenerators(content, inheritance);
+
+			string[] interestedCoroutines =
+			{
+				"ManageItemCRUDService::createItem",
+				"CoCoMEOrderProducts::makeNewOrder",
+				"CoCoMEOrderProducts::orderItem",
+				"CoCoMEOrderProducts::placeOrder",
+				"CoCoMESystem::receiveOrderedProduct",
+			};
+			string[] lowPriorityCoroutines =
+			{
+				"ManageItemCRUDService::deleteItem",
+			};
 
 
 			var bindings = new Dictionary<PaperVariable, PaperWord>();
