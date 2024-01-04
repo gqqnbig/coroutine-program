3afac6ad51b1da71418fd92c3b07dbc09ca29a96
QQ喵 <gqqnb2005@gmail.com>
2024-01-04 12:30:06 +0800

    find coroutines in a go file

--- Go/prebuild.bat
+++ Go/prebuild.bat
@@ -2,5 +2,5 @@ echo "%ProjectDir%"
 cd /d "%ProjectDir%"
 set CLASSPATH=.;%ProjectDir%..\antlr-4.13.0-complete.jar;%CLASSPATH%
 
-call ..\antlr4.bat -Dlanguage=CSharp -no-listener -visitor -o generated -package GoLang.Antlr GoLexer.g4 GoParser.g4
+call ..\antlr4.bat -Dlanguage=CSharp -visitor -o generated -package GoLang.Antlr GoLexer.g4 GoParser.g4
 
