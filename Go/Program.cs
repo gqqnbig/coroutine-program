b46962409b0ce0a96049e7932cf23eccc85f3626
QQ喵 <gqqnb2005@gmail.com>
2024-01-04 12:30:06 +0800

    find coroutines in a go file

--- Go/Program.cs
+++ Go/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 namespace Go
 {
@@ -7,8 +8,7 @@ namespace Go
 	{
 		static void Main(string[] args)
 		{
-
-			string path = @"E:\GeneratorCalculation\Go\tests\channels.go";
+			string path = @"E:\GeneratorCalculation\Go\tests\func.go";
 			var stream = CharStreams.fromPath(path);
 			GoLang.Antlr.GoLexer lexer = new GoLang.Antlr.GoLexer(stream);
 			CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -16,8 +16,12 @@ namespace Go
 
 			var tree = parser.sourceFile();
 
-			Console.WriteLine(tree.ChildCount);
+			//Console.WriteLine(tree.ChildCount);
 			Console.WriteLine("Hello World!");
+
+			GoStatementListener l = new GoStatementListener();
+			ParseTreeWalker walker = new ParseTreeWalker();
+			walker.Walk(l, tree);
 		}
 	}
 }
