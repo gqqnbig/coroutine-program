97807d866c8ab08fe38fdf71da82602278b4e0c1
QQ喵 <gqqnb2005@gmail.com>
2024-01-04 11:42:16 +0800

    Add parser for golang

---
+++ Go/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace Go
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string path = @"E:\GeneratorCalculation\Go\tests\channels.go";
+			var stream = CharStreams.fromPath(path);
+			GoLang.Antlr.GoLexer lexer = new GoLang.Antlr.GoLexer(stream);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			GoLang.Antlr.GoParser parser = new GoLang.Antlr.GoParser(tokens);
+
+			var tree = parser.sourceFile();
+
+			Console.WriteLine(tree.ChildCount);
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
