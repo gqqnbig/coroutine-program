97dfdcbf3ae7043cea897589657694dab63b91b8
QQ喵 <gqqnb2005@gmail.com>
2024-01-04 17:06:07 +0800

    find coroutine definition

---
+++ Go/ParameterTypeVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+using GoLang.Antlr;
+
+namespace Go
+{
+	class ParameterTypeVisitor: GoParserBaseVisitor<bool>
+	{
+		public Dictionary<string, string> channelTypes = new Dictionary<string, string>();
+
+		public override bool VisitParameterDecl([NotNull] GoParser.ParameterDeclContext context)
+		{
+			string ct = context.type_().typeLit()?.channelType()?.elementType().GetText();
+			if (ct != null)
+			{
+				var ids = context.identifierList().IDENTIFIER();
+				foreach (var id in ids)
+				{
+					channelTypes.Add(id.GetText(), ct);
+				}
+
+			}
+
+			return true;
+		}
+	}
+}
