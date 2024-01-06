c7cc4d336a9951291b90388728e27922cf37cba4
QQ喵 <gqqnb2005@gmail.com>
2024-01-06 11:21:18 +0800

    Read send and receive statements

--- Go/ParameterTypeVisitor.cs
+++ Go/ParameterTypeVisitor.cs
@@ -6,13 +6,13 @@ using GoLang.Antlr;
 
 namespace Go
 {
-	class ParameterTypeVisitor: GoParserBaseVisitor<bool>
+	class ParameterTypeVisitor : GoParserBaseVisitor<bool>
 	{
 		public Dictionary<string, string> channelTypes = new Dictionary<string, string>();
 
 		public override bool VisitParameterDecl([NotNull] GoParser.ParameterDeclContext context)
 		{
-			string ct = context.type_().typeLit()?.channelType()?.elementType().GetText();
+			string ct = GetChannelType(context.type_());
 			if (ct != null)
 			{
 				var ids = context.identifierList().IDENTIFIER();
@@ -24,6 +24,12 @@ namespace Go
 			}
 
 			return true;
+		}
+
+		public static string GetChannelType(GoParser.Type_Context context)
+		{
+			string ct = context.typeLit()?.channelType()?.elementType().GetText();
+			return ct;
 		}
 	}
 }
