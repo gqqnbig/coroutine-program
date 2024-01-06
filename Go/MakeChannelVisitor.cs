180e089c11f07cb80344d8dcad9564ea3135e749
QQ喵 <gqqnb2005@gmail.com>
2024-01-06 11:21:18 +0800

    Read send and receive statements

---
+++ Go/MakeChannelVisitor.cs
@@ -0,0 +1,23 @@
+﻿using Antlr4.Runtime.Misc;
+using GoLang.Antlr;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Go
+{
+	class MakeChannelVisitor : GoParserBaseVisitor<bool>
+	{
+		public string type = null;
+
+		public override bool VisitPrimaryExpr([NotNull] GoParser.PrimaryExprContext context)
+		{
+			if (context.primaryExpr()?.GetText() == "make")
+			{
+				type = ParameterTypeVisitor.GetChannelType(context.arguments().type_());
+				return true;
+			}
+			return base.VisitPrimaryExpr(context);
+		}
+	}
+}
