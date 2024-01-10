89cfe3c79e8377c4da2d5d85f25fc2a14b630b7b
QQ喵 <gqqnb2005@gmail.com>
2024-01-10 17:31:53 +0800

    If a coroutine definition is called, it turns into a coroutine instance

---
+++ Go/StartFunction.cs
@@ -0,0 +1,24 @@
+﻿using GeneratorCalculation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Go
+{
+	class StartFunction : FunctionType
+	{
+		public StartFunction(CoroutineDefinitionType definition) : base("Start", definition)
+		{
+		}
+
+		public override PaperWord ApplyEquation(List<KeyValuePair<PaperVariable, PaperWord>> equations)
+		{
+			return new StartFunction((CoroutineDefinitionType)Arguments[0]).Evaluate();
+		}
+
+		public override PaperWord Evaluate()
+		{
+			return ((CoroutineDefinitionType)base.Arguments[0]).Start();
+		}
+	}
+}
