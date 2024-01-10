35776cb7e6aa7e14205143c08d3bde44b835daac
QQ喵 <gqqnb2005@gmail.com>
2024-01-10 16:30:32 +0800

    fix building script

--- Go/prebuild.sh
+++ Go/prebuild.sh
@@ -6,6 +6,6 @@ export CLASSPATH=".:${ProjectDir}../antlr-4.13.0-complete.jar:${CLASSPATH}"
 shopt -s expand_aliases
 alias antlr4='java org.antlr.v4.Tool'
 
-antlr4 -Dlanguage=CSharp -no-listener -visitor -o generated -package DiffSyntax.Antlr REModel.g4
+antlr4 -Dlanguage=CSharp -listener -visitor -o generated -package GoLang.Antlr GoLexer.g4 GoParser.g4
 # antlr4.bat -Dlanguage=CSharp -no-listener -visitor -o generated -package DiffSyntax.Antlr  JavaLexer.g4 JavaParser.g4
 
