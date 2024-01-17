a1203002e401433356374a5a80c4c69c000f474c
QQ喵 <gqqnb2005@gmail.com>
2024-01-17 19:28:49 +0800

    Add method CheckDeadlock()

---
+++ GeneratorCalculation/Binding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneratorCalculation
+{
+	public class Bindings
+	{
+		Dictionary<PaperVariable, PaperWord> dict = new Dictionary<PaperVariable, PaperWord>();
+
+		public void Add(string key, PaperWord value)
+		{
+			dict.Add(new PaperVariable(key), value);
+		}
+
+
+		public void Add(PaperVariable key, PaperWord value)
+		{
+			dict.Add(key, value);
+		}
+
+		public Dictionary<PaperVariable, PaperWord> GetDict()
+		{
+			return dict;
+		}
+	}
+}
