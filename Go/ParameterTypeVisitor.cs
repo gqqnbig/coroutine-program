c590a1dfe3d3ae9bea87cfa2d139cd67c6d5c36c
QQ喵 <gqqnb2005@gmail.com>
2024-02-12 17:12:36 +0800

    Add comments

--- Go/ParameterTypeVisitor.cs
+++ Go/ParameterTypeVisitor.cs
@@ -26,6 +26,12 @@ namespace Go
 			return true;
 		}
 
+		/// <summary>
+		/// Returns the type of the channel. 
+		/// If it's not a channel type, return null.
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
 		public static string GetChannelType(GoParser.Type_Context context)
 		{
 			string ct = context.typeLit()?.channelType()?.elementType().GetText();
