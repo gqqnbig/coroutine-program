1f1d0ea1a1674a1e3d8ef3d82e01e42b6b483dc2
QQ喵 <gqqnb2005@gmail.com>
2024-02-16 12:38:13 +0800

    Support function call within send/receive

---
+++ Go/FuncInfo.cs
@@ -0,0 +1,21 @@
+﻿using GeneratorCalculation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Go
+{
+	class FuncInfo
+	{
+		public CoroutineDefinitionType CoroutineType { get; set; }
+		public string ChannelType { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (obj is FuncInfo objF)
+				return object.Equals(CoroutineType, objF.CoroutineType) && object.Equals(ChannelType, objF.ChannelType);
+
+			return false;
+		}
+	}
+}
